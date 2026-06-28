d277c7b5aa38db409fd640ef0bd675f08f0abad1
AxiomaticAtom <logicalaxiomlab@gmail.com>
2026-06-28 11:10:48 +0100

    Update CourseData.js

--- scripts/CourseData.js
+++ scripts/CourseData.js
@@ -10,6 +10,7 @@ let typesOfParticles = [
   new Page("Atomic Structure", "../content/atomicStructure/internalStructureOfTheAtom.html"),
   new Page("Subatomic Particles", "../content/atomicStructure/propertiesOfSubatomicParticles.html"),
   new Page("Charges", "../content/atomicStructure/howChargesInteract.html"),
+  new Page("Other Particles", "../content/atomicStructure/otherParticles.html"),
   new Page("Elements and Compounds", "../content/atomicStructure/elementsAndCompounds.html" ),
   new Page("Mixtures vs Pure Substances", "../content/atomicStructure/mixtures.html" )
 ];
